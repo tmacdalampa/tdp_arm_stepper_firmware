65907864874167b77852cf21a8aef0aba98671d3
tmacdalampa <44917465+tmacdalampa@users.noreply.github.com>
2019-05-21 11:37:41 +0800

    try again
    try again step by step

--- servo_motor.py
+++ servo_motor.py
@@ -8,19 +8,12 @@ class ServoMotor(object):
     def __init__(self, control_pin, pwm_freq):
         self.CONTROL_PIN = control_pin
         self.PWM_FREQ = pwm_freq
-        #self.STEP= STEP
 
     def begin(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.CONTROL_PIN, GPIO.OUT)
-        pwm = GPIO.PWM(self.CONTROL_PIN, self.PWM_FREQ)
-
-    def angle_to_PWM(self, angle):
-        pwm = GPIO.PWM(self.CONTROL_PIN, self.PWM_FREQ)
-        pwm.start(0)
+        
+    def angle_to_duty_cycle(self, angle):
         duty_cycle = (0.05 * self.PWM_FREQ) + (0.2 * self.PWM_FREQ * angle / 180)
-        dc=pwm.ChangeDutyCycle(duty_cycle)
-        pwm.stop()
-        GPIO.cleanup()
         return dc
 
--- servo_test_j4.py
+++ servo_test_j4.py
@@ -9,12 +9,19 @@ servo4=servo_motor.ServoMotor(control_pin=17, pwm_freq=50)
 
 servo4.begin()
 
+pwm=GPIO.PWM(servo4.CONTROL_PIN, self.PWM_FREQ)
+pwm.start(0)
+
 import numpy as np
 
 theta=[5, -5, 10, -10, 45, -45, 90, -90, 180, -180]
 for i in range(0,8):
   t0=time.time()
-  print(servo4.angle_to_PWM(theta[i]))
+  dc=servo4.angle_to_duty_cycle(theta[i])
+  pwm.ChangeDutyCycle(dc)
+  print(angle[i])
   sleep(1)
 
+pwm.stop()
+GPIO.cleanup()
 servo4.disable()
